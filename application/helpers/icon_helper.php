d9960f08876a040d5b7783ce7a5391394f6f94a3
gunarsp <gunars28@inbox.lv>
2014-07-28 22:11:04 +0300

    rollback

--- application/helpers/icon_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function icon($icon_name, $alt='', $ext='png') {
-
-	return '<img src="' . base_url() . 'assets/style/img/icons/' . $icon_name . '.' . $ext . '" alt="' . $alt . '" />';
-
-}
-
-?>
