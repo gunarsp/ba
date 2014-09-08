60971fa023e798dd8d427ea34d4c2e722bd59da9
gunarsp <gunars28@inbox.lv>
2014-09-08 21:48:43 +0300

    custom

---
+++ application/modules_custom/export/config/config.php
@@ -0,0 +1,12 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+$config = array(
+	'module_path'			=>	'export',
+	'module_name'			=>	'Export',
+	'module_description'	=>	'Export data from businessassistant.',
+	'module_author'			=>	'Jesse Terry',
+	'module_homepage'		=>	'http://www.businessassistant.com',
+	'module_version'		=>	'0.9.3'
+);
+
+?>
