415756b8298ef957eecf4853e167097f10bbdc0f
gunarsp <gunars28@inbox.lv>
2014-09-10 21:04:35 +0300

    ba_modules

---
+++ application/logs/log-2014-09-08.php
@@ -0,0 +1,101 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-09-08 21:01:31 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Hooks Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Utf8 Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> UTF-8 Support Enabled
+DEBUG - 2014-09-08 21:01:31 --> URI Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Router Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> No URI present. Default controller set.
+DEBUG - 2014-09-08 21:01:31 --> Output Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Security Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Input Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-09-08 21:01:31 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Controller Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Dashboard MX_Controller Initialized
+DEBUG - 2014-09-08 21:01:31 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Session Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Helper loaded: string_helper
+DEBUG - 2014-09-08 21:01:31 --> A session cookie was not found.
+DEBUG - 2014-09-08 21:01:31 --> Session routines successfully run
+DEBUG - 2014-09-08 21:01:31 --> Helper loaded: url_helper
+DEBUG - 2014-09-08 21:01:31 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Hooks Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Utf8 Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> UTF-8 Support Enabled
+DEBUG - 2014-09-08 21:01:31 --> URI Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Router Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Output Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Security Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Input Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-09-08 21:01:31 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Controller Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Session Class Initialized
+DEBUG - 2014-09-08 21:01:31 --> Helper loaded: string_helper
+DEBUG - 2014-09-08 21:01:31 --> Session routines successfully run
+DEBUG - 2014-09-08 21:01:31 --> Database Driver Class Initialized
+ERROR - 2014-09-08 21:01:31 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/ba/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-09-08 21:01:31 --> Helper loaded: url_helper
+DEBUG - 2014-09-08 21:01:32 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Hooks Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Utf8 Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> UTF-8 Support Enabled
+DEBUG - 2014-09-08 21:01:32 --> URI Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Router Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Output Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Security Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Input Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-09-08 21:01:32 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Language Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Config Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Loader Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Controller Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Language file loaded: language/english/ba_lang.php
+DEBUG - 2014-09-08 21:01:32 --> Helper loaded: url_helper
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/setup/models/mdl_setup.php
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/ba_data/models/mdl_ba_data.php
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/ba_modules/models/mdl_ba_modules.php
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/invoices/models/mdl_invoice_amounts.php
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/fields/models/mdl_fields.php
+DEBUG - 2014-09-08 21:01:32 --> Model Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Session Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> Helper loaded: string_helper
+DEBUG - 2014-09-08 21:01:32 --> Session routines successfully run
+DEBUG - 2014-09-08 21:01:32 --> Helper loaded: form_helper
+DEBUG - 2014-09-08 21:01:32 --> Form Validation Class Initialized
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/setup/views/header.php
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/setup/views/license_agreement.php
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/setup/views/footer.php
+DEBUG - 2014-09-08 21:01:32 --> File loaded: application/modules_core/setup/views/index.php
+DEBUG - 2014-09-08 21:01:32 --> Final output sent to browser
+DEBUG - 2014-09-08 21:01:32 --> Total execution time: 0.0735
+DEBUG - 2014-09-08 21:02:30 --> Config Class Initialized
+DEBUG - 2014-09-08 21:02:30 --> Hooks Class Initialized
+DEBUG - 2014-09-08 21:02:30 --> Utf8 Class Initialized
+DEBUG - 2014-09-08 21:02:30 --> UTF-8 Support Enabled
+DEBUG - 2014-09-08 21:02:30 --> URI Class Initialized
+DEBUG - 2014-09-08 21:02:30 --> Router Class Initialized
+ERROR - 2014-09-08 21:02:30 --> 404 Page Not Found --> 
+DEBUG - 2014-09-08 21:02:36 --> Config Class Initialized
+DEBUG - 2014-09-08 21:02:36 --> Hooks Class Initialized
+DEBUG - 2014-09-08 21:02:36 --> Utf8 Class Initialized
+DEBUG - 2014-09-08 21:02:36 --> UTF-8 Support Enabled
+DEBUG - 2014-09-08 21:02:36 --> URI Class Initialized
+DEBUG - 2014-09-08 21:02:36 --> Router Class Initialized
+ERROR - 2014-09-08 21:02:36 --> 404 Page Not Found --> 
